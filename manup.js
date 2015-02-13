c2595bf41b857b00ab567e5f9225da7f5ea6aef7
jeffburt <boyofgreen@gmail.com>
2015-02-12 22:16:59 -0600

    add support for display prop to map to iOS meta tag

--- manup.js
+++ manup.js
@@ -26,7 +26,7 @@ var generateFullMetaData = function(){
           }
       }else{
           validMetaValues[i].content = manUpObject[validMetaValues[i].manifestName];
-          if(validMetaValues[i].manifestName == 'display' && manUpObject.display == 'standalone']) validMetaValues[i].content == 'yes'
+          if(validMetaValues[i].manifestName == 'display' && manUpObject.display == 'standalone') validMetaValues[i].content == 'yes'
       }
   } 
  };
